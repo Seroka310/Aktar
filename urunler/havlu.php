000878e80434f4be97c75162019997003f10af42
Seroka310 <62809381+Seroka310@users.noreply.github.com>
2020-03-28 21:49:29 +0300

    Add files via upload

---
+++ urunler/havlu.php
@@ -0,0 +1,67 @@
+<?php require("../header.php"); ?>
+<?php require("../menu.php"); ?>
+	
+	<div id="wrapper" class="toogled">
+        <div class="overlay" style="display: block;"></div>
+    
+        <!-- Sidebar -->
+        <nav class="navbar navbar-inverse navbar-fixed-top" id="sidebar-wrapper" role="navigation">
+            <ul class="nav sidebar-nav">
+                <li class="sidebar-brand">
+                    <a href="#">
+                       ÜRÜN ÇEŞİTLERİ
+                    </a>
+                </li>
+                <li>
+                    <a href="#">Havlu</a>
+                </li>
+                <li>
+                    <a href="#">Nevresim</a>
+                </li>
+                <li>
+                    <a href="#">Çarşaf</a>
+                </li>
+                <li>
+                    <a href="#">Bornoz</a>
+                </li>
+                
+                <li>
+                    <a href="#">Services</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+                <li>
+                    <a href="https://twitter.com/maridlcrmn">Follow me</a>
+                </li>
+            </ul>
+        </nav>
+        <!-- /#sidebar-wrapper -->
+
+        <!-- Page Content -->
+        <div id="page-content-wrapper">
+            <button type="button" class="hamburger is-closed" data-toggle="offcanvas">
+                <span class="hamb-top"></span>
+    			<span class="hamb-middle"></span>
+				<span class="hamb-bottom"></span>
+            </button>
+            <div class="container">
+                <div class="row">
+                    <div class="col-lg-8 col-lg-offset-2">
+                       Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                                   tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
+                                   quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
+                                   consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse
+                                   cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non
+                                   proident, sunt in culpa qui officia deserunt mollit anim id est laborum.            
+                                   
+                    </div>
+                </div>
+            </div>
+        </div>
+        <!-- /#page-content-wrapper -->
+
+    </div>
+
+
+<?php require("../footer.php"); ?>
